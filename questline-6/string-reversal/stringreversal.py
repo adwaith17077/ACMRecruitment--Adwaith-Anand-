5400defea523ce0d82791135670d9a0a72a42f38
adwaith17077 <adwaithanand.anandam@gmail.com>
2025-09-09 17:11:17 +0530

    Add files via upload

---
+++ questline-6/string-reversal/stringreversal.py
@@ -0,0 +1,20 @@
+
+# Iterative method
+def iterative(s):
+    r = ""
+    for i in s:
+        r =i+r 
+    return r
+
+
+# Recursive method
+def recursive(s):
+    return s[::-1] 
+
+
+s = "unibic"
+print("Original string:", s)
+print("Reversed (Iterative):", iterative(s))
+print("Reversed (Recursive):", recursive(s))
+
+
